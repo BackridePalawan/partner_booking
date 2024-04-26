4b402ed120aecc458a86838ce5d4062f38ebb4ec
blanns456 <blannslapening@gmail.com>
2024-04-26 15:34:42 +0800

    login and register ui

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#7cae41",
+        "primary-dark": "#4c6c33",
+        secondary: "#1f3f56",
+      },
+    },
+  },
+  plugins: [],
+};
